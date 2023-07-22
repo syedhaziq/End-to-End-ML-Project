751014d004670d88ae0c569152faf4cadee0506c
Haziq <syed_haziq@hotmail.com>
2023-07-22 16:50:00 +0500

    Model Trainer added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from data_transformation import DataTransformation
 from data_transformation import DataTransformationConfig
+from model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -49,5 +50,8 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
+    data_transformation = DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
